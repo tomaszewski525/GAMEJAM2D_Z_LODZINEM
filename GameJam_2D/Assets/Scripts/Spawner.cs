fd43fbc650e8309f0a7aa7f4387113fa57eadbea
tomaszewski525 <75726290+tomaszewski525@users.noreply.github.com>
2022-11-13 05:41:08 +0100

    Kurwa

--- GameJam_2D/Assets/Scripts/Spawner.cs
+++ GameJam_2D/Assets/Scripts/Spawner.cs
@@ -6,53 +6,137 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject[] enemies;
+    public GameObject player;
+    ScoreKeeper scoreKeeper;
+    public Object[] enemies;
     public float verticalBorder;
     public float horizontalBorder;
     public float[] probabilities;
     public float[] secProbabilities;
-    List<KeyValuePair<GameObject, float>> elements;
-    float time = 5.0f;
+
+    List<KeyValuePair<Object, float>> elements;
+    //float time = 5.0f;
+    Spawner_Handler spawner_Handler;
+
+    public float delayTime;
+    public int delayScore;
+    public int timeOnStage;
+
+
+    int playerScore;
+
+    int stage = 1;
+    //int nextStageScore = 10;
+
+    float spawnerTime;
+
+    //int playerScore_cp = 0;
+
+    int newStage = 0;
+
+    //bool startSpawn = false;
+
+    float spawnTimer;
+    public void Update()
+    {
+        delayTime -= Time.deltaTime;
+        playerScore = scoreKeeper.score;
+
+        print(playerScore);
+
+        if (delayTime < 0 || playerScore > delayScore)
+        {
+            spawnerTime += Time.deltaTime;
+            //print(spawnerTime % timeOnStage);
+
+            if (timeOnStage - spawnerTime < 0)
+            {
+                spawnerTime -= timeOnStage;
+
+                stage = stage + 1;
+
+            }
+
+            if (newStage != stage)
+            {
+                //spawner_Handler = Instantiate(spawner_Handler, transform.position, transform.rotation);
+                newStage = stage;
+                //startSpawn = true;
+            }
+
+        }
+
+
+        if(stage != 0 && (delayTime < 0 || playerScore > delayScore))
+        {
+            float time = (float)timeOnStage / (float)stage;
+            time = Mathf.Clamp(time, 0.5f, 10.0f);
+
+            spawnTimer += Time.deltaTime;
+            if (spawnTimer> time)
+            {
+                Spawn();
+                spawnTimer = 0;
+            }
+        }
+
+    }
+
 
     public void Spawn()
     {
+
         Object enemy = Choose();
         int index = Random.Range(0, 3);
         Transform ins = enemy.GetComponent<Transform>().GetChild(index);
-        Vector2 randomSpawnPos = new Vector2(Random.Range(-verticalBorder, verticalBorder), Random.Range(-horizontalBorder, horizontalBorder));
-        Instantiate(ins, randomSpawnPos, Quaternion.identity);
+        float x_offset = Random.Range(-2, 2);
+        float y_offset = Random.Range(-2, 2);
+
+
+        Vector2 randomSpawnPos = new Vector2(transform.position.x + x_offset, transform.position.y + y_offset);
+
+        //Vector2 randomSpawnPos = new Vector2(Random.Range(-verticalBorder, verticalBorder), Random.Range(-horizontalBorder, horizontalBorder));
+        // Quaternion.identity
+        Instantiate(ins, randomSpawnPos, transform.rotation);
     }
 
+    /*
     IEnumerator ISpawner()
     {
         while (true)
         {
-            Mathf.Clamp(time, 2.0f, 4.0f);
+            time = timeOnStage / stage;
+
+            // Mathf.Clamp(time, 2.0f, 4.0f);
             Spawn();
             yield return new WaitForSeconds(time);
         }
-    }
+    }*/
 
+    /*
     void IncreseSpeed(Enemy e)
     {
         time -= 0.2f;
-    }
+    }*/
 
     private void Start()
     {
-        Enemy.OnEnemyDeath += IncreseSpeed;
-        elements = new List<KeyValuePair<GameObject, float>>();
+        scoreKeeper = player.GetComponent<ScoreKeeper>();
+
+        //Enemy.OnEnemyDeath += IncreseSpeed;
+        elements = new List<KeyValuePair<Object, float>>();
         for (int i = 0; i < enemies.Length; i++)
         {
-            elements.Add(new KeyValuePair<GameObject, float>(enemies[i], probabilities[i]));
+            elements.Add(new KeyValuePair<Object, float>(enemies[i], probabilities[i]));
         }
-        StartCoroutine(ISpawner());
+
+        //;
     }
     Object Choose()
     {
+
         System.Random r = new System.Random();
         float diceRoll = (float)(r.NextDouble());
-
         float cumulative = 0f;
         for (int i = 0; i < elements.Count; i++)
         {
@@ -62,6 +146,6 @@ public class Spawner : MonoBehaviour
                 return elements[i].Key;
             }
         }
-        return new GameObject();
+        return new Object();
     }
 }
